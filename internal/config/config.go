de195158f1931842f100c8dae219abbbf5c904e8
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-02-02 20:47:35 -0500

    Added initial versions of Read function and changed getConfigFilePath to use os and filepath calls

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,18 +3,39 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+
+	// "io"
+	"os"
+	"path/filepath"
 )
 
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	db_url	string
-	current_user_name	string
+	Database	string	`json:"db_url"`
+	CurrentUser	string	`json:"current_user_name"`
 }
 
 func Read() Config {
 	// reads the JSON file found at ~/.gatorconfig.json and returns a Config struct
+	file, err := getConfigFilePath()
+	fmt.Printf("Loading config file from: %v\n", file)
+	if err != nil {
+		fmt.Println("Error on filepath")
+		return Config{}
+	}
+	
+	body, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error on file read")
+		return Config{}
+	}
+
+	var data Config
+	if err := json.Unmarshal(body, &data); err != nil {
+		return Config{}
+	}
+	return data
 }
 
 func SetUser(c Config) {
@@ -23,7 +44,13 @@ func SetUser(c Config) {
 }
 
 func getConfigFilePath() (string, error) {
-	var path = "~./" + configFileName
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	var path = filepath.Join(homeDir, configFileName)
+
 	return path, nil
 }
 
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/fatbotgw/gator/internal/config"
+)
+
 func main()  {
-	
+	gatorConfig := config.Read()
+
+	fmt.Printf("Config: %v\n", gatorConfig)
+
 }
